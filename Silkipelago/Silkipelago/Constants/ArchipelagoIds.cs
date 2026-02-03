66624d809afaff34388f2fcab87864fd7d508449
Louis-Roch <noxus72@gmail.com>
2026-02-02 21:42:33 -0500

    adding reversed List

--- Silkipelago/Silkipelago/Constants/ArchipelagoIds.cs
+++ Silkipelago/Silkipelago/Constants/ArchipelagoIds.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using static Silkipelago.Constants.PlayerDataStrings;
 
 
@@ -6,18 +7,26 @@ namespace Silkipelago.Constants
 {
     public static class ArchipelagoIds
     {
-        public static Dictionary<string, string> Items { get; } = new Dictionary<string, string>
+        public static Dictionary<string, string> ArchipelagoIdsToGameIds { get; } = new Dictionary<string, string>
     {
+        //abilities
         { "Drifter's Cloak", HAS_DRIFTER_CLOAK },
         { "Cling Grip", HAS_WALL_JUMP },
         { "Swift Step", HAS_DASH },
         { "Faydown Cloak", HAS_DOUBLE_JUMP },
         { "Clawline", HAS_HARPOON_DASH }
+        //bosses
     };
+        public static Dictionary<string, string> GameIdsToArchipelagoIds = ArchipelagoIdsToGameIds.ToDictionary(keyValuePair => keyValuePair.Value, keyValuePair => keyValuePair.Key);
 
         public static string GetInGameName(string archipelagoName)
         {
-            return Items.TryGetValue(archipelagoName, out var value) ? value : null;
+            return ArchipelagoIdsToGameIds.TryGetValue(archipelagoName, out var value) ? value : null;
+        }
+
+        public static string GetArchipelagoName(string inGameName)
+        {
+            return GameIdsToArchipelagoIds.TryGetValue(inGameName, out var value) ? value : null;
         }
     }
 }
